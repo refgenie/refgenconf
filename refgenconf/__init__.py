9e06d902fdbea32af3d202d970b9a7ced3d203a1
nsheff <nsheff@users.noreply.github.com>
2019-05-14 16:37:06 -0400

    fix imports

--- refgenconf/__init__.py
+++ refgenconf/__init__.py
@@ -1,6 +1,6 @@
 from ._version import __version__
 
-from .refgenie import RefGenomeConfiguration, load_yaml
+from .refgenconf import RefGenomeConfiguration, load_yaml
 
 __classes__ = ["RefGenomeConfiguration"]
 __all__ = ["RefGenomeConfiguration", "load_yaml"]

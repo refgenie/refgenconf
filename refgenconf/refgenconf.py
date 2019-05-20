3068bc507354505e86bb0ea52ad9e1a8700e287d
Vince Reuter <vince.reuter@gmail.com>
2019-05-20 16:14:03 -0400

    docstrings; flexibility for assets_str

--- refgenconf/refgenconf.py
+++ refgenconf/refgenconf.py
@@ -65,24 +65,64 @@ class RefGenomeConfiguration(yacman.YacAttMap):
         """
         return {g: list(self.genomes[g].keys()) for g in self.genomes}
 
-    def assets_str(self):
+    def assets_str(self, offset_text="  ", asset_sep="; ",
+                   genome_assets_delim=": "):
         """
+        Create a block of text representing genome-to-asset mapping.
 
-        :return:
+        :param str offset_text: text that begins each line of the text
+            representation that's produced
+        :param str asset_sep: the delimiter between names of types of assets,
+            within each genome line
+        :param str genome_assets_delim: the delimiter to place between
+            reference genome assembly name and its list of asset names
+        :return str: text representing genome-to-asset mapping
         """
-        string = ""
-        for genome, values in self.genomes.items():
-            string += "  {}: {}\n".format(genome, "; ".join(list(values)))
-        return string
+        def make_line(gen, assets):
+            return offset_text + "{}{}{}".format(
+                gen, genome_assets_delim, asset_sep.join(list(assets)))
+        return "\n".join([make_line(g, am) for g, am in self.genomes.items()])
 
     def list_assets_by_genome(self, genome=None):
+        """
+        List types/names of assets that are available for one--or all--genomes.
+
+        :param str | NoneType genome: reference genome assembly ID, optional;
+            if omitted, the full mapping from genome to asset names
+        :return Iterable[str] | Mapping[str, Iterable[str]]: collection of
+            asset type names available for particular reference assembly if
+            one is provided, else the full mapping between assembly ID and
+            collection available asset type names
+        """
         return self.assets_dict() if genome is None else list(self.genomes[genome].keys())
 
     def list_genomes_by_asset(self, asset=None):
+        """
+        List assemblies for which a particular asset is available.
+
+        :param str | NoneType asset: name of type of asset of interest, optional
+        :return Iterable[str] | Mapping[str, Iterable[str]]: collection of
+            assemblies for which the given asset is available; if asset
+            argument is omitted, the full mapping from name of asset type to
+            collection of assembly names for which the asset key is available
+            will be returned.
+        """
         return self._invert_genomes() \
             if not asset else [g for g, am in self.genomes if asset in am]
 
     def _invert_genomes(self):
+        """ Map each asset type/kind/name to a collection of assemblies.
+
+        A configuration file encodes assets by genome, but in some use cases
+        it's helpful to invert the direction of this mapping. The value of the
+        asset key/name may differ by genome, so that information is
+        necessarily lost in this inversion, but we can collect genome IDs by
+        asset ID.
+
+        :return Mapping[str, Iterable[str]] binding between asset kind/key/name
+            and collection of reference genome assembly names for which the
+            asset type is available
+        """
         genomes = {}
         for g, am in self.genomes.items():
             for a in am.keys():

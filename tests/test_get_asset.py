08bf5ee3237306b8910e76943d57e6a86229cfb3
Vince Reuter <vince.reuter@gmail.com>
2019-05-24 12:47:29 -0400

    test existence check function responsiveness

--- tests/test_get_asset.py
+++ tests/test_get_asset.py
@@ -5,6 +5,7 @@ import pytest
 from refgenconf import *
 from tests.conftest import get_conf_genomes, CONF_DATA, HG38_DATA, MM10_DATA, \
     MITO_DATA
+from veracitools import ExpectContext
 
 __author__ = "Vince Reuter"
 __email__ = "vreuter@virginia.edu"
@@ -49,21 +50,26 @@ def test_check_exist_param_type(rgc, check_exist, gname, aname):
         rgc.get_asset(gname, aname, check_exist=check_exist)
 
 
-@pytest.mark.parametrize(
-    ["strict", "ctxmgr", "error"], [(False, pytest.warns, RuntimeWarning), (True, pytest.raises, IOError)])
-def test_existence_check_strictness(rgc, tmpdir, strict, ctxmgr, error):
-    """ Strict asset existence check raises IOError iff path existence fails. """
+@pytest.fixture
+def temp_asset_spec(tmpdir):
     fn = "semaphore.txt"
     fp = tmpdir.join(fn).strpath
     assert not os.path.exists(fp)
-    gname = "tmpgen"
-    aname = "testasset"
-    rgc.genomes[gname] = {aname: fp}
+    return fp
+
+
+@pytest.mark.parametrize(
+    ["strict", "ctxmgr", "error"],
+    [(False, pytest.warns, RuntimeWarning), (True, pytest.raises, IOError)])
+def test_existence_check_strictness(rgc, temp_asset_spec, strict, ctxmgr, error):
+    """ Asset existence check behavior responds to strictness parameter. """
+    gname, aname = "tmpgen", "testasset"
+    rgc.genomes[gname] = {aname: temp_asset_spec}
     def fetch():
         return _get_asset(rgc, gname, aname, strict_exists=strict)
     with ctxmgr(error):
         fetch()
-    with open(fp, 'w'):
+    with open(temp_asset_spec, 'w'):
         pass
     try:
         fetch()
@@ -71,12 +77,28 @@ def test_existence_check_strictness(rgc, tmpdir, strict, ctxmgr, error):
         pytest.fail(str(e))
 
 
-@pytest.mark.skip("not implemented")
-def test_existence_check_function():
-    pass
+@pytest.mark.parametrize(
+    ["check_exist", "get_exp_from_path"],
+    [(os.path.isfile, lambda p: p), (os.path.isdir, lambda _: IOError)])
+def test_existence_check_function(
+        rgc, check_exist, get_exp_from_path, temp_asset_spec):
+    """ Asset existence check behavior responds to existence checker. """
+    gname, aname = "tmpgen", "testasset"
+    rgc.genomes[gname] = {aname: temp_asset_spec}
+    with open(temp_asset_spec, 'w'):
+        pass
+    with ExpectContext(get_exp_from_path(temp_asset_spec), _get_asset) as ctx:
+        ctx(rgc, gname, aname, check_exist=check_exist, strict_exists=True)
 
 
 def _get_asset(rgc, g, a, **kwargs):
+    """
+    Call the asset fetch function.
+
+    :param refgenconf.RefGenConf rgc: configuration instance
+    :param str g: genome name
+    :param str a: asset name
+    """
     kwds = {"strict_exists": None}
     kwds.update(kwargs)
     return rgc.get_asset(g, a, **kwds)

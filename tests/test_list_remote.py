1765b68d31953b9c051fa2ea7d6214a7308e1a33
Vince <vince.reuter@gmail.com>
2019-05-31 02:34:04 -0400

    condense

--- tests/test_list_remote.py
+++ tests/test_list_remote.py
@@ -13,10 +13,8 @@ def test_list_remote(rgc):
     with mock.patch("refgenconf.refgenconf._read_remote_data",
                     return_value=rgc.genomes):
         genomes, assets = new_rgc.list_remote(get_url=lambda _: "irrelevant")
-    exp_genomes = rgc.genomes_str()
-    exp_assets = rgc.assets_str()
-    _assert_eq_as_sets(exp_genomes, genomes)
-    _assert_eq_as_sets(exp_assets, assets)
+    _assert_eq_as_sets(rgc.genomes_str(), genomes)
+    _assert_eq_as_sets(rgc.assets_str(), assets)
 
 
 def _assert_eq_as_sets(a, b):

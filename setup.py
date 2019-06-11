b457c33e03851efd83493347d83d1c34ad3ba16a
Vince <vince.reuter@gmail.com>
2019-06-11 09:40:52 -0400

    build on and advertise 2.7

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7", 
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Scientific/Engineering :: Bio-Informatics"

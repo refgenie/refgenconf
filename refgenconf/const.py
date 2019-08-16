cad79708f9fd5fd0a732928c31d8fa472292f3b2
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-08-16 11:40:06 -0400

    update consts

--- refgenconf/const.py
+++ refgenconf/const.py
@@ -10,6 +10,7 @@ CFG_NAME = "genome configuration"
 CFG_ENV_VARS = ["REFGENIE"]
 CFG_CONST = ["CFG_NAME", "CFG_ENV_VARS"]
 DEFAULT_SERVER = "http://refgenomes.databio.org"
+DEFAULT_TAG_NAME = "default"
 
 CFG_FOLDER_KEY = "genome_folder"
 CFG_SERVER_KEY = "genome_server"
@@ -17,58 +18,72 @@ CFG_ARCHIVE_KEY = "genome_archive"
 CFG_VERSION_KEY = "config_version"
 CFG_GENOMES_KEY = "genomes"
 
-CFG_CHECKSUM_KEY = "checksum"
-CFG_CONTENTS_KEY = "contents"
+CFG_CHECKSUM_KEY = "genome_checksum"
 CFG_GENOME_DESC_KEY = "genome_description"
-CFG_ASSETS_KEY = "assets"
+CFG_ASSETS_KEY = "asset_packages"
 
-CFG_ASSET_PATH_KEY = "path"
+CFG_ASSET_PATH_KEY = "asset_path"
 CFG_ASSET_SIZE_KEY = "asset_size"
 CFG_ASSET_DESC_KEY = "asset_description"
 CFG_ARCHIVE_SIZE_KEY = "archive_size"
 CFG_ARCHIVE_CHECKSUM_KEY = "archive_checksum"
+CFG_SEEK_KEYS_KEY = "seek_keys"
 
 CFG_TOP_LEVEL_KEYS = [
     CFG_FOLDER_KEY, CFG_SERVER_KEY, CFG_ARCHIVE_KEY, CFG_GENOMES_KEY, CFG_VERSION_KEY]
 CFG_GENOME_KEYS = [
-    CFG_GENOME_DESC_KEY, CFG_ASSETS_KEY, CFG_CONTENTS_KEY, CFG_CHECKSUM_KEY]
-CFG_GENOME_ATTRS_KEYS = [CFG_GENOME_DESC_KEY, CFG_CHECKSUM_KEY, CFG_CONTENTS_KEY]
-CFG_SINGLE_ASSET_SECTION_KEYS = [
-    CFG_ASSET_PATH_KEY, CFG_ASSET_DESC_KEY, CFG_ASSET_SIZE_KEY, CFG_ARCHIVE_SIZE_KEY, CFG_ARCHIVE_CHECKSUM_KEY]
+    CFG_GENOME_DESC_KEY, CFG_ASSETS_KEY, CFG_CHECKSUM_KEY]
+CFG_GENOME_ATTRS_KEYS = [CFG_GENOME_DESC_KEY, CFG_CHECKSUM_KEY]
+CFG_SINGLE_ASSET_SECTION_KEYS = [CFG_ASSET_PATH_KEY, CFG_ASSET_DESC_KEY, CFG_ASSET_SIZE_KEY, CFG_ARCHIVE_SIZE_KEY, CFG_ARCHIVE_CHECKSUM_KEY, CFG_SEEK_KEYS_KEY]
 
 CFG_KEY_NAMES = [
     "CFG_FOLDER_KEY", "CFG_SERVER_KEY", "CFG_GENOMES_KEY",
-    "CFG_ASSET_PATH_KEY", "CFG_ASSET_DESC_KEY", "CFG_ARCHIVE_KEY", "CFG_ARCHIVE_SIZE_KEY",
-    "CFG_ASSET_SIZE_KEY", "CFG_CHECKSUM_KEY", "CFG_ARCHIVE_CHECKSUM_KEY", "CFG_VERSION_KEY", "CFG_CONTENTS_KEY"]
+    "CFG_ASSET_PATH_KEY", "CFG_ASSET_DESC_KEY", "CFG_ARCHIVE_KEY", "CFG_ARCHIVE_SIZE_KEY", "CFG_SEEK_KEYS_KEY",
+    "CFG_ASSET_SIZE_KEY", "CFG_CHECKSUM_KEY", "CFG_ARCHIVE_CHECKSUM_KEY", "CFG_VERSION_KEY"]
 
 
 """
 # example genome configuration structure
-{version}: 0.2
+{version}: 0.3
 {folder}: $GENOMES
 {server}: http://localhost
 {archive}: /path/to/archives
 
 {genomes}:
-  hg38:
-    {desc_genome}: Reference assembly GRCh38, released in Dec 2013
-    {checksum}: 1110349234n20349280345df5035
-    {contents}:
-        chr1: mm20349234n20349280345mv2035
-    {assets}:
-        bowtie2:
-          {path}: indexed_bowtie2
-          {desc_asset}: Genome index for bowtie2, produced with bowtie2-build
-          {archive_checksum}: 2220349234n20349280345mv2035
-          {asset_size}: 32G
-          {archive_size}: 7G
+    hg38:
+        {desc_genome}: Reference assembly GRCh38, released in Dec 2013
+        {checksum}: 1110349234n20349280345df5035
+        {assets}:
+            fasta:
+                default:
+                    {asset_path}: fasta
+                    {desc_asset}: Genome index for bowtie2, produced with bowtie2-build
+                    {archive_checksum}: 2220349234n20349280345mv2035
+                    {asset_size}: 32G
+                    {archive_size}: 7G
+                    {seek_keys}:
+                        fasta: hg38.fa.gz
+                        fai: hg38.fa.fai
+                        chrom_sizes: sizes.txt
+                old_version:
+                    {asset_path}: fasta
+                    {desc_asset}: Genome index for bowtie2, produced with bowtie2-build
+                    {archive_checksum}: 4545n49234n2034928dmasn4n5jn5
+                    {asset_size}: 32G
+                    {archive_size}: 7G
+                    {seek_keys}:
+                        fasta: hg38.fa.gz
+                        fai: hg38.fa.fai
+                        chrom_sizes: sizes.txt
 """.format(folder=CFG_FOLDER_KEY, server=CFG_SERVER_KEY, version=CFG_VERSION_KEY, assets=CFG_ASSETS_KEY,
-           archive=CFG_ARCHIVE_KEY, checksum=CFG_CHECKSUM_KEY, contents=CFG_CONTENTS_KEY, genomes=CFG_GENOMES_KEY,
-           desc_genome=CFG_GENOME_DESC_KEY, path=CFG_ASSET_PATH_KEY, desc_asset=CFG_ASSET_DESC_KEY,
-           archive_checksum=CFG_ARCHIVE_CHECKSUM_KEY, asset_size=CFG_ASSET_SIZE_KEY, archive_size=CFG_ARCHIVE_SIZE_KEY)
+           archive=CFG_ARCHIVE_KEY, checksum=CFG_CHECKSUM_KEY, genomes=CFG_GENOMES_KEY,
+           desc_genome=CFG_GENOME_DESC_KEY, asset_path=CFG_ASSET_PATH_KEY, desc_asset=CFG_ASSET_DESC_KEY,
+           archive_checksum=CFG_ARCHIVE_CHECKSUM_KEY, asset_size=CFG_ASSET_SIZE_KEY, archive_size=CFG_ARCHIVE_SIZE_KEY,
+           seek_keys=CFG_SEEK_KEYS_KEY)
 
 # other consts
-REQ_CFG_VERSION = 0.2
+REQ_CFG_VERSION = 0.3
+REFGENIE_BY_CFG = {"0.3": "0.6.1", "0.2": "0.6.0"}  # should probably switch to 0.7.0
 
-__all__ = CFG_CONST + CFG_KEY_NAMES + ["DEFAULT_SERVER", "CFG_KEY_NAMES", "CFG_GENOME_DESC_KEY", "REQ_CFG_VERSION",
-                                       "CFG_ASSETS_KEY", "CFG_GENOME_ATTRS_KEYS"]
+__all__ = CFG_CONST + CFG_KEY_NAMES + ["DEFAULT_SERVER", "DEFAULT_TAG_NAME", "CFG_KEY_NAMES", "CFG_GENOME_DESC_KEY",
+                                       "REQ_CFG_VERSION", "CFG_ASSETS_KEY", "CFG_GENOME_ATTRS_KEYS", "REFGENIE_BY_CFG"]

7fc1470a4ebcfdc9f22cf425be7aa52862244ae5
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-08-21 20:52:44 -0400

    implement setting default pointer method

--- refgenconf/refgenconf.py
+++ refgenconf/refgenconf.py
@@ -243,6 +243,20 @@ class RefGenConf(yacman.YacAttMap):
                               .format(CFG_ASSET_DEFAULT_TAG_KEY, genome, asset, alt), RuntimeWarning)
                 return alt
 
+    def set_default_pointer(self, genome, asset, tag):
+        """
+        Point to the selected tag by default
+
+        :param str genome: name of a reference genome assembly of interest
+        :param str asset: name of the particular asset of interest
+        :param str tag: name of the particular asset tag to point to by default
+        """
+        _assert_gat_exists(self[CFG_GENOMES_KEY], genome, asset)
+        if not hasattr(self[CFG_GENOMES_KEY][genome][CFG_ASSETS_KEY][asset], CFG_ASSET_DEFAULT_TAG_KEY) or \
+                len(self[CFG_GENOMES_KEY][genome][CFG_ASSETS_KEY][asset][CFG_ASSET_DEFAULT_TAG_KEY]) == 0:
+            self[CFG_GENOMES_KEY][genome][CFG_ASSETS_KEY][asset][CFG_ASSET_DEFAULT_TAG_KEY] = tag
+            _LOGGER.info("default tag for '{}/{}' changed to: {}".format(genome, asset, tag))
+
     def list_assets_by_genome(self, genome=None, order=None):
         """
         List types/names of assets that are available for one--or all--genomes.

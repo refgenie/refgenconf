2adc671d5a17d37aaf1053863d8b9f553eb12f17
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2019-10-19 13:49:29 -0500

    prep release

--- tests/test_1pull_asset.py
+++ tests/test_1pull_asset.py
@@ -4,19 +4,14 @@ import logging
 import mock
 import os
 import sys
-import time
 if sys.version_info.major < 3:
-    from urllib2 import HTTPError
     ConnectionRefusedError = Exception
 else:
     from urllib.error import HTTPError
 import pytest
-from yacman import YacAttMap
-import refgenconf
 from refgenconf.const import *
 from refgenconf.exceptions import *
 from refgenconf.refgenconf import _download_url_progress
-import ubiquerg
 
 __author__ = "Vince Reuter"
 __email__ = "vreuter@virginia.edu"
@@ -45,10 +40,8 @@ def test_negative_response_to_large_download_prompt(rgc, gname, aname, tname):
     """ Test responsiveness to user abortion of pull request. """
     with mock.patch("refgenconf.refgenconf._is_large_archive", return_value=True), \
          mock.patch("refgenconf.refgenconf.query_yes_no", return_value=False):
-        res = rgc.pull_asset(gname, aname, tname)
-    key, val = _parse_single_pull(res)
-    assert aname == key[1]
-    assert val is None
+        gat, archive_dict = rgc.pull_asset(gname, aname, tname)
+    assert gat == [gname, aname, tname]
 
 
 @pytest.mark.parametrize(["gname", "aname", "tname"],
@@ -72,27 +65,19 @@ def test_download_interruption(my_rgc, gname, aname, tname, caplog):
     assert "The download was interrupted" in r.msg
 
 
-@pytest.mark.parametrize(["gname", "aname", "tname"], [("rCRSd", "fasta", "default"), ("mouse_chrM2x", "fasta", "default")])
+@pytest.mark.parametrize(["gname", "aname", "tname"], [("human_repeats", "fasta", "default"), ("mouse_chrM2x", "fasta", "default")])
 def test_pull_asset(my_rgc, gname, aname, tname):
     with mock.patch("refgenconf.refgenconf.query_yes_no", return_value=True):
         print("\nPulling; genome: {}, asset: {}, tag: {}\n".format(gname, aname, tname))
         my_rgc.pull_asset(gname, aname, tname)
 
 
-def _parse_single_pull(result):
-    """ Unpack asset pull result, expecting asset name and value. """
-    try:
-        k, v = result
-    except (IndexError, ValueError):
-        print("Single pull result should be a list with one pair; got {}".
-              format(result))
-        raise
-    return k, v
-
-
-@pytest.mark.parametrize(["gname", "aname", "tname"], [("mouse_chrM2x", "bwa_index", "default")])
+@pytest.mark.parametrize(["gname", "aname", "tname"],
+                         [("rCRSd", "bowtie2_index", "default"), ("mouse_chrM2x", "bwa_index", "default")])
 def test_parent_asset_mismatch(my_rgc, gname, aname, tname):
     """ Test that an exception is raised when remote and local parent checksums do not match on pull"""
+    with mock.patch("refgenconf.refgenconf.query_yes_no", return_value=True):
+        my_rgc.pull_asset(gname, "fasta", tname)
     my_rgc[CFG_GENOMES_KEY][gname][CFG_ASSETS_KEY]["fasta"][CFG_ASSET_TAGS_KEY][tname][CFG_ASSET_CHECKSUM_KEY] = "wrong"
-    with pytest.raises(RemoteDigestMismatchError):
+    with pytest.raises(RefgenconfError):
         my_rgc.pull_asset(gname, aname, tname)

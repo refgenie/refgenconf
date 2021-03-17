51683577ca0ba0b4835beb6163df1692bc78a993
nsheff <nsheff@users.noreply.github.com>
2021-03-17 13:52:06 -0400

    lint

--- refgenconf/__init__.py
+++ refgenconf/__init__.py
@@ -7,9 +7,19 @@ from .helpers import *
 from .refgenconf import *
 from .populator import populate_refgenie_refs, looper_refgenie_plugin
 
-__all__ = ["RefGenConf", "select_genome_config", "get_dir_digest",
-           "GenomeConfigFormatError", "MissingAssetError",
-           "MissingConfigDataError", "MissingGenomeError", "RefgenconfError",
-           "UnboundEnvironmentVariablesError"] + ["DEFAULT_SERVER"] + \
-          CFG_KEY_NAMES + \
-          ["populate_refgenie_refs", "looper_refgenie_plugin"]
+__all__ = (
+    [
+        "RefGenConf",
+        "select_genome_config",
+        "get_dir_digest",
+        "GenomeConfigFormatError",
+        "MissingAssetError",
+        "MissingConfigDataError",
+        "MissingGenomeError",
+        "RefgenconfError",
+        "UnboundEnvironmentVariablesError",
+    ]
+    + ["DEFAULT_SERVER"]
+    + CFG_KEY_NAMES
+    + ["populate_refgenie_refs", "looper_refgenie_plugin"]
+)
--- refgenconf/populator.py
+++ refgenconf/populator.py
@@ -8,13 +8,14 @@ from ubiquerg import parse_registry_path as prp
 
 _LOGGER = logging.getLogger(__name__)
 
+
 def looper_refgenie_plugin(namespaces):
     """
     A looper plugin that populates refgenie references in a PEP from
     refgenie://genome/asset:tag registry paths. This can be used to convert
     all refgenie references into their local paths at the looper stage, so the
     final paths are passed to the workflow. This way the workflow does not
-    need to depend on refgenie to resolve the paths. 
+    need to depend on refgenie to resolve the paths.
     This is useful for example for CWL pipelines, which are built to have
     paths resolved outside the workflow.
 
@@ -26,19 +27,19 @@ def looper_refgenie_plugin(namespaces):
     return populate_refgenie_refs(rgc, namespaces)
 
 
-
 # Example code:
 # import refgenconf as RGC
 # rgc = RGC.RefGenConf("/home/nsheff/Dropbox/env/refgenie_config/zither.yaml")
 # rgc.seek("hg19", "fasta")
-# demo = {"genome": 'refgenie://hg19/fasta', 
-#         "other_attr": "something", 
+# demo = {"genome": 'refgenie://hg19/fasta',
+#         "other_attr": "something",
 #         "bt2": 'refgenie://t7/bowtie2_index'}
 # nested_demo = {"top_level_attr": "refgenie://t7/fasta",
 #                 "other_top_attr": "don't do anything to this",
-#                 "nested_dict": demo }        
+#                 "nested_dict": demo }
 # populate_refgenie_refs(rgc, demo)
 # populate_refgenie_refs(rgc, nested_demo)
+
 
 def populate_refgenie_refs(rgc, glob):
     """
@@ -50,7 +51,7 @@ def populate_refgenie_refs(rgc, glob):
         paths. Dict include nested dicts.
     :return dict: modified input dict with refgenie paths populated
     """
-    p = re.compile('refgenie://([A-Za-z0-9_/\.]+)?')
+    p = re.compile("refgenie://([A-Za-z0-9_/\.]+)?")
 
     if isinstance(glob, str):
         m = p.match(glob)
@@ -61,17 +62,25 @@ def populate_refgenie_refs(rgc, glob):
             # print(reg_path)
             rgpkg = prp(reg_path)
             if not rgpkg:
-                _LOGGER.info("Can't convert non-conforming refgenie registry path: {}".format(reg_path))
+                _LOGGER.info(
+                    "Can't convert non-conforming refgenie registry path: {}".format(
+                        reg_path
+                    )
+                )
                 return glob
-            rgpath = rgc.seek(rgpkg["namespace"], rgpkg["item"], rgpkg["tag"], rgpkg["subitem"])
+            rgpath = rgc.seek(
+                rgpkg["namespace"], rgpkg["item"], rgpkg["tag"], rgpkg["subitem"]
+            )
             glob = re.sub(reg_path, rgpath, glob)
         return glob
     elif isinstance(glob, dict):
-        for k,v in glob.items():
+        for k, v in glob.items():
             # print(k, v)
-            if k.startswith("_"): continue
-            if k.startswith("sources"): continue # derived attribute sources
-            glob[k] = populate_refgenie_refs(rgc, v)    
+            if k.startswith("_"):
+                continue
+            if k.startswith("sources"):
+                continue  # derived attribute sources
+            glob[k] = populate_refgenie_refs(rgc, v)
             # if k == "project": continue
         return glob
     else:

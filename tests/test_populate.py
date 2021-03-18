9fc238b044cb9a087541fd27f2cfa85653b74cab
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2021-03-18 11:05:51 -0400

    test if result is correct in populate method

--- tests/test_populate.py
+++ tests/test_populate.py
@@ -1,12 +1,13 @@
-""" Tests for RefGenConf.getseq. These tests depend on successful completion of tests is test_1pull_asset.py """
+""" Tests for RefGenConf.populate. These tests depend on successful completion of tests is test_1pull_asset.py """
 
 import pytest
-from pyfaidx import FastaRecord, Sequence
 
 
 class TestPopulate:
     @pytest.mark.parametrize(
-        ["gname", "chr"], [("rCRSd", "rCRSd"), ("human_repeats", "U14567.1")]
+        ["gname", "aname"], [("rCRSd", "fasta"), ("human_repeats", "fasta")]
     )
-    def test_populate(self, ro_rgc, gname, chr):
-        print(ro_rgc.populate("refgenie://rCRSd/fasta"))
+    def test_populate(self, ro_rgc, gname, aname):
+        assert ro_rgc.populate(f"refgenie://{gname}/{aname}") == ro_rgc.seek(
+            genome_name=gname, asset_name=aname
+        )
